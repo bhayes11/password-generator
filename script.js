7f15b6421b53fc02a7bbff2a3d878a8cf8e2fa6d
Hayes11 <bhayes81@gmail.com>
2020-09-19 20:56:08 -0400

    added generatePassword to resolve error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-console.log("hello");
+//console.log("hello");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +9,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+function generatePassword(){
+  return "this will be replaced by my generated password.";
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
