5d2158c20c4b1a0069169001196559cb0bf42056
Hayes11 <bhayes81@gmail.com>
2020-09-22 23:19:52 -0400

    converted arrays to strings

--- script.js
+++ script.js
@@ -4,24 +4,21 @@ var generateBtn = document.querySelector("#generate");
 //console.log("hello");
 
 //arrays
-var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "`", "{", "|", "}", "~"];
-var numChar = [0,1,2,3,4,5,6,7,8,9];
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!#[]$%&'()*+,-./:;<=>?@^`{|}~]"
+var numChar = "0123456789";
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
   prompt("Enter the length of characters for your password. Choose between 8 and 128 characters.");
-  confirm("Would you like to use uppercase Letters?")
-  confirm("Would you like to use lowercase letters?")
-  confirm("Would you like to use special characters?")
-  confirm("Would you like to use numbers?")
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+for(var i = 0; i < specialChar.length; i++){
+  console.log(specialChar[i]);
 }
-
 
 function generatePassword(){
   return "this will be replaced by my generated password.";
