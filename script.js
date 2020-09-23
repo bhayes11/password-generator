f78e5ddc6e9d06c00d7d60565126d1eeff353f0d
Hayes11 <bhayes81@gmail.com>
2020-09-23 00:03:19 -0400

    added alert for character length

--- script.js
+++ script.js
@@ -3,35 +3,59 @@
 
 //console.log("hello");
 
-//arrays
+//variables
 var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var specialChar = "!#[]$%&'()*+,-./:;<=>?@^`{|}~]"
+const symbols = "!#[]$%&'()*+,-./:;<=>?@^`{|}~]"
 var numChar = "0123456789";
+var possibleCharacters = "";
 var password = "";
 var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
-var possibleCharacters = "";
 
 
 // Write password to the #password input
 function writePassword() {
-  prompt("Enter the length of characters for your password. Choose between 8 and 128 characters.");
+  characterAmount = prompt("Enter the length of characters for your password. Choose between 8 and 128 characters.");
+  if (characterAmount < 8 || characterAmount > 120); {
+    alert("Choose between 8 to 128 characters");
+  }
 
+  var includeUppercase = confirm("Would you like to use uppercase Letters?");
 
-for(var i = 0; i < specialChar.length; i++){
-  console.log(specialChar[i]);
+  var includeLowercase = confirm("Would you like to use lowercase letters?");
+
+  var includeNumbers = confirm("Would you like to use special characters?");
+
+  var includeSymbols = confirm("Would you like to use numbers?");
+
+  if (includeUppercase) {
+    possibleCharacters += upperChar;
+  }
+
+  if (includeLowercase) {
+    possibleCharacters += lowerChar;
+  }
+  if (includeNumbers) {
+    possibleCharacters += numChar;
+  }
+
+  if (includeSymbols) {
+    possibleCharacters += symbols;
+  }
+  
+  for (var i = 0; i < characterAmount; i++) {
+  password += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+  }
+  passwordText.value = password;
 }
 
-function generatePassword(){
-  return "this will be replaced by my generated password.";
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //GIVEN I need a new, secure password
 //WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria. 
+//THEN I am presented with a series of prompts for password criteria.
 
 //WHEN prompted for password criteria
 //THEN I select which criteria to include in the password
