ef341173acb4b150afb2ce03c160bf6fcedbac71
Hayes11 <bhayes81@gmail.com>
2020-09-23 00:19:18 -0400

    added return to length prompt

--- script.js
+++ script.js
@@ -17,10 +17,15 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   characterAmount = prompt("Enter the length of characters for your password. Choose between 8 and 128 characters.");
-  if (characterAmount < 8 || characterAmount > 120); {
+  if (characterAmount < 8) {
     alert("Choose between 8 to 128 characters");
+    return writePassword();
   }
-
+  else if (characterAmount > 128) {
+    alert("Choose between 8 to 128 characters");
+    return writePassword();
+  }
+  
   var includeUppercase = confirm("Would you like to use uppercase Letters?");
 
   var includeLowercase = confirm("Would you like to use lowercase letters?");
